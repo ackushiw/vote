203aa8e07d5743cb951143a1a58a8041e5f03717
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2018-01-01 01:23:49 -0500

    updated votes reset watcher

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,6 +25,8 @@ export default ({ store }) => {
       })
       console.log('ARRAY', votes)
       store.commit('setVotes', votes)
+    } else {
+      store.commit('setVotes', [])
     }
   })
 }
